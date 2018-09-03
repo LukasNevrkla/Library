b869c4debdfc47c6d3e45c61c0d2f78fc98211ef
LukasNevrkla <42738049+LukasNevrkla@users.noreply.github.com>
2018-09-03 20:56:00 +0200

    new_json_file_in_net_core

---
+++ Library_Manager/Library_Manager/Models/LibraryContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Library_Manager.Models
+{
+    public partial class LibraryContext : DbContext
+    {
+        public LibraryContext()
+        {
+        }
+
+        public LibraryContext(DbContextOptions<LibraryContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Books> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Books>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Author).HasMaxLength(20);
+
+                entity.Property(e => e.Name).HasMaxLength(20);
+
+                entity.Property(e => e.PublicationDate).HasColumnName("Publication_date");
+
+                entity.Property(e => e.Translation).HasMaxLength(20);
+            });
+        }
+    }
+}
